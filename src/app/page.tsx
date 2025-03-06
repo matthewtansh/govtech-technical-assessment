319dc45e81fe437e5f1a681c538734d1b57872c4
matthewtansh <156554782+matthewtansh@users.noreply.github.com>
2025-03-06 11:28:57 +0800

    feat: complete initial version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const initialUsers = [
+  { id: 1, name: "Amy" },
+  { id: 2, name: "Bravo" },
+  { id: 3, name: "Charlie" },
+];
+
+export default function UserForm() {
+  const [users, setUsers] = useState<User[]>(initialUsers);
+
+  const [newName, setNewName] = useState("");
+
+  const handleAddUser = () => {
+    if (newName.trim()) {
+      setUsers([...users, { id: Date.now(), name: newName }]);
+      setNewName("");
+    }
+  };
+
+  const handleUpdateUser = (id: number, name: string) => {
+    setUsers(
+      users.map((user) => (user.id === id ? { ...user, name: name } : user)),
+    );
+  };
+
+  const handleDeleteUser = (id: number) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-auto mt-10 max-w-md border-2 border-black p-[15px]">
+      {/* Name */}
+      <div>Name</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      {/* Input and Add Button */}
+      <div className="mt-[5px] mb-[20px]">
+        <input
+          className="w-[30%] rounded-sm border border-neutral-400 bg-white px-2 py-1"
+          type="text"
+          value={newName}
+          placeholder="Enter new name"
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <button
+          className="ml-[10px] rounded-sm bg-[#7CB9E8] px-2 py-1"
+          onClick={handleAddUser}
+        >
+          Add
+        </button>
+      </div>
+
+      {/* List of Users */}
+      <div className="mb-[10px]">List of Users</div>
+      <div>
+        {users.map((user) => (
+          <div className="mb-[10px]" key={user.id}>
+            {/* User */}
+            <input
+              className="w-[30%] rounded-sm border border-neutral-600 bg-white px-2 py-1 text-center"
+              type="text"
+              value={user.name}
+              onChange={(e) => handleUpdateUser(user.id, e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+            {/* Delete Button */}
+            <button
+              className="ml-[10px] rounded-sm bg-red-400 px-2 py-1"
+              onClick={() => handleDeleteUser(user.id)}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
